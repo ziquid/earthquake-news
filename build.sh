83ddbcf67f207f44185f220d02bd059f0e015c93
Joseph Cheek <joseph@gdgwebworks.com>
2016-04-18 14:31:06 -0400

    minor build system updates

--- build.sh
+++ build.sh
@@ -9,9 +9,12 @@ echo "$PWD" | grep -s -q htdocs && SCOPE=prod
 echo "$PWD" | grep -s -q dev && SCOPE=dev
 
 chmod ug+w sites/default
+cp .gitignore gitignore
 $DRUSH -y -q make gdg.make
+mv gitignore .gitignore
 $DRUSH -y en master features
+echo enabling modules with a scope of $SCOPE
 $DRUSH -y master-execute --scope=$SCOPE
 $DRUSH -y fra
 $DRUSH -y updb
-$DRUSH cc all
+# $DRUSH cc all
