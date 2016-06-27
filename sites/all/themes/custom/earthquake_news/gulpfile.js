06f21a34d45cfe0cec99199535a7a4215259bea0
Joseph Cheek <joseph@gdgwebworks.com>
2016-06-27 15:21:39 -0400

    rel=nofollow and some display tweaks

--- sites/all/themes/custom/earthquake_news/gulpfile.js
+++ sites/all/themes/custom/earthquake_news/gulpfile.js
@@ -1,21 +1,8 @@
 var gulp       = require('gulp');
 var less       = require('gulp-less');
-var watch      = require('gulp-watch');
-var livereload = require('gulp-livereload');
-var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('less', function () {
   gulp.src('less/style.less')
-    .pipe(sourcemaps.init())
     .pipe(less())
-    .pipe(sourcemaps.write())
     .pipe(gulp.dest('css'))
-   .pipe(livereload());
 });
-
-/*
-gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('less/*.less', ['less']);
-});
-*/
